85213010ac5bcde0ce4d7884a2885a5ee64dcfe2
TaeKim <tae@xreators.com>
2022-05-24 00:08:12 +0900

    ### Removed some functions - freeze functions - pausable functions - upgradable functions

--- contracts/ORT.sol
+++ contracts/ORT.sol
@@ -110,12 +110,13 @@ contract Ownable {
         require(msg.sender == owner);
         _;
     }
+
     modifier onlyNewOwner() {
         require(msg.sender != address(0));
         require(msg.sender == newOwner);
         _;
     }
-    
+
     function isOwner(address account) public view returns (bool) {
         if( account == owner ){
             return true;
@@ -131,13 +132,13 @@ contract Ownable {
     }
 
     function acceptOwnership() public onlyNewOwner returns(bool) {
-        emit OwnershipTransferred(owner, newOwner);        
+        emit OwnershipTransferred(owner, newOwner);
         owner = newOwner;
         newOwner = address(0);
     }
 }
 
-contract PauserRole is Ownable{
+contract PauserRole is Ownable {
     using Roles for Roles.Role;
 
     event PauserAdded(address indexed account);
@@ -161,7 +162,7 @@ contract PauserRole is Ownable{
     function addPauser(address account) public onlyPauser {
         _addPauser(account);
     }
-    
+
     function removePauser(address account) public onlyOwner {
         _removePauser(account);
     }
@@ -178,56 +179,6 @@ contract PauserRole is Ownable{
     function _removePauser(address account) internal {
         _pausers.remove(account);
         emit PauserRemoved(account);
-    }
-}
-
-contract Pausable is PauserRole {
-    event Paused(address account);
-    event Unpaused(address account);
-
-    bool private _paused;
-
-    constructor () internal {
-        _paused = false;
-    }
-
-    /**
-     * @return true if the contract is paused, false otherwise.
-     */
-    function paused() public view returns (bool) {
-        return _paused;
-    }
-
-    /**
-     * @dev Modifier to make a function callable only when the contract is not paused.
-     */
-    modifier whenNotPaused() {
-        require(!_paused);
-        _;
-    }
-
-    /**
-     * @dev Modifier to make a function callable only when the contract is paused.
-     */
-    modifier whenPaused() {
-        require(_paused);
-        _;
-    }
-
-    /**
-     * @dev called by the owner to pause, triggers stopped state
-     */
-    function pause() public onlyPauser whenNotPaused {
-        _paused = true;
-        emit Paused(msg.sender);
-    }
-
-    /**
-     * @dev called by the owner to unpause, returns to normal state
-     */
-    function unpause() public onlyPauser whenPaused {
-        _paused = false;
-        emit Unpaused(msg.sender);
     }
 }
 
@@ -390,67 +341,9 @@ contract ERC20 is IERC20 {
         _balances[account] = _balances[account].add(value);
         emit Transfer(address(0), account, value);
     }
-
-    /**
-     * @dev Internal function that burns an amount of the token of a given
-     * account.
-     * @param account The account whose tokens will be burnt.
-     * @param value The amount that will be burnt.
-     */
-    function _burn(address account, uint256 value) internal {
-        require(account != address(0));
-
-        _totalSupply = _totalSupply.sub(value);
-        _balances[account] = _balances[account].sub(value);
-        emit Transfer(account, address(0), value);
-    }
-
-    /**
-     * @dev Internal function that burns an amount of the token of a given
-     * account, deducting from the sender's allowance for said account. Uses the
-     * internal burn function.
-     * Emits an Approval event (reflecting the reduced allowance).
-     * @param account The account whose tokens will be burnt.
-     * @param value The amount that will be burnt.
-     */
-    function _burnFrom(address account, uint256 value) internal {
-        _allowed[account][msg.sender] = _allowed[account][msg.sender].sub(value);
-        _burn(account, value);
-        emit Approval(account, msg.sender, _allowed[account][msg.sender]);
-    }
 }
 
-
-
-contract ERC20Pausable is ERC20, Pausable {
-    function transfer(address to, uint256 value) public whenNotPaused returns (bool) {
-        return super.transfer(to, value);
-    }
-
-    function transferFrom(address from, address to, uint256 value) public whenNotPaused returns (bool) {
-        return super.transferFrom(from, to, value);
-    }
-    
-    /*
-     * approve/increaseApprove/decreaseApprove can be set when Paused state
-     */
-     
-    /*
-     * function approve(address spender, uint256 value) public whenNotPaused returns (bool) {
-     *     return super.approve(spender, value);
-     * }
-     *
-     * function increaseAllowance(address spender, uint addedValue) public whenNotPaused returns (bool success) {
-     *     return super.increaseAllowance(spender, addedValue);
-     * }
-     *
-     * function decreaseAllowance(address spender, uint subtractedValue) public whenNotPaused returns (bool success) {
-     *     return super.decreaseAllowance(spender, subtractedValue);
-     * }
-     */
-}
-
-contract ERC20Detailed is IERC20 {
+contract ERC20Detailed is ERC20 {
     string private _name;
     string private _symbol;
     uint8 private _decimals;
@@ -483,111 +376,67 @@ contract ERC20Detailed is IERC20 {
     }
 }
 
-contract ORT is ERC20Detailed, ERC20Pausable {
-    
+contract ORT is ERC20Detailed, PauserRole {
+
     struct LockInfo {
         uint256 _releaseTime;
         uint256 _amount;
     }
-    
-    address public implementation;
 
     mapping (address => LockInfo[]) public timelockList;
-  	mapping (address => bool) public frozenAccount;
-    
-    event Freeze(address indexed holder);
-    event Unfreeze(address indexed holder);
+
     event Lock(address indexed holder, uint256 value, uint256 releaseTime);
     event Unlock(address indexed holder, uint256 value);
 
-    modifier notFrozen(address _holder) {
-        require(!frozenAccount[_holder]);
-        _;
-    }
-    
-    constructor() ERC20Detailed("ORT", "ORT", 18) public  {
-        
+    constructor() ERC20Detailed("XREATORS", "ORT", 18) public  {
         _mint(msg.sender, 500000000 * (10 ** 18));
-
     }
-    
+
     function balanceOf(address owner) public view returns (uint256) {
-        
         uint256 totalBalance = super.balanceOf(owner);
         if( timelockList[owner].length >0 ){
             for(uint i=0; i<timelockList[owner].length;i++){
                 totalBalance = totalBalance.add(timelockList[owner][i]._amount);
             }
         }
-        
+
         return totalBalance;
     }
-    
-    function transfer(address to, uint256 value) public notFrozen(msg.sender) returns (bool) {
+
+    function transfer(address to, uint256 value) public returns (bool) {
         if (timelockList[msg.sender].length > 0 ) {
-            _autoUnlock(msg.sender);            
+            _autoUnlock(msg.sender);
         }
         return super.transfer(to, value);
     }
 
-    function transferFrom(address from, address to, uint256 value) public notFrozen(from) returns (bool) {
+    function transferFrom(address from, address to, uint256 value) public returns (bool) {
         if (timelockList[from].length > 0) {
-            _autoUnlock(from);            
+            _autoUnlock(from);
         }
         return super.transferFrom(from, to, value);
     }
-    
-    function freezeAccount(address holder) public onlyPauser returns (bool) {
-        require(!frozenAccount[holder]);
-        frozenAccount[holder] = true;
-        emit Freeze(holder);
-        return true;
-    }
 
-    function unfreezeAccount(address holder) public onlyPauser returns (bool) {
-        require(frozenAccount[holder]);
-        frozenAccount[holder] = false;
-        emit Unfreeze(holder);
-        return true;
-    }
-    
-    function lock(address holder, uint256 value, uint256 releaseTime) public onlyPauser returns (bool) {
-        require(_balances[holder] >= value,"There is not enough balances of holder.");
-        _lock(holder,value,releaseTime);
-        
-        
-        return true;
-    }
-    
     function transferWithLock(address holder, uint256 value, uint256 releaseTime) public onlyPauser returns (bool) {
         _transfer(msg.sender, holder, value);
         _lock(holder,value,releaseTime);
         return true;
     }
-    
+
     function unlock(address holder, uint256 idx) public onlyPauser returns (bool) {
         require( timelockList[holder].length > idx, "There is not lock info.");
         _unlock(holder,idx);
         return true;
     }
-    
-    /**
-     * @dev Upgrades the implementation address
-     * @param _newImplementation address of the new implementation
-     */
-    function upgradeTo(address _newImplementation) public onlyOwner {
-        require(implementation != _newImplementation);
-        _setImplementation(_newImplementation);
-    }
-    
+
     function _lock(address holder, uint256 value, uint256 releaseTime) internal returns(bool) {
         _balances[holder] = _balances[holder].sub(value);
         timelockList[holder].push( LockInfo(releaseTime, value) );
-        
+
         emit Lock(holder, value, releaseTime);
         return true;
     }
-    
+
     function _unlock(address holder, uint256 idx) internal returns(bool) {
         LockInfo storage lockinfo = timelockList[holder][idx];
         uint256 releaseAmount = lockinfo._amount;
@@ -595,13 +444,13 @@ contract ORT is ERC20Detailed, ERC20Pausable {
         delete timelockList[holder][idx];
         timelockList[holder][idx] = timelockList[holder][timelockList[holder].length.sub(1)];
         timelockList[holder].length -=1;
-        
+
         emit Unlock(holder, releaseAmount);
         _balances[holder] = _balances[holder].add(releaseAmount);
-        
+
         return true;
     }
-    
+
     function _autoUnlock(address holder) internal returns(bool) {
         for(uint256 idx =0; idx < timelockList[holder].length ; idx++ ) {
             if (timelockList[holder][idx]._releaseTime <= now) {
@@ -612,34 +461,5 @@ contract ORT is ERC20Detailed, ERC20Pausable {
             }
         }
         return true;
-    }
-
-    /**
-     * @dev Sets the address of the current implementation
-     * @param _newImp address of the new implementation
-     */
-    function _setImplementation(address _newImp) internal {
-        implementation = _newImp;
-    }
-    
-    /**
-     * @dev Fallback function allowing to perform a delegatecall 
-     * to the given implementation. This function will return 
-     * whatever the implementation call returns
-     */
-    function () payable external {
-        address impl = implementation;
-        require(impl != address(0));
-        assembly {
-            let ptr := mload(0x40)
-            calldatacopy(ptr, 0, calldatasize)
-            let result := delegatecall(gas, impl, ptr, calldatasize, 0, 0)
-            let size := returndatasize
-            returndatacopy(ptr, 0, size)
-            
-            switch result
-            case 0 { revert(ptr, size) }
-            default { return(ptr, size) }
-        }
     }
 }
